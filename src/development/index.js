16faaca70e1f019e2ce29aad4f97dd29d7e4f077
Ztj <ztj1993@gmail.com>
2020-02-08 00:26:34 +0800

    feat: development

---
+++ src/development/index.js
@@ -0,0 +1,3 @@
+if (process.env.NODE_ENV === 'development') {
+    //TODO: 引入当前目录下除了 index 的所有文件
+}
--- src/entries/app.js
+++ src/entries/app.js
@@ -4,6 +4,7 @@ import router from '../router'
 import store from '../store'
 
 import '../utils'
+import '../development'
 
 Vue.config.productionTip = false;
 
