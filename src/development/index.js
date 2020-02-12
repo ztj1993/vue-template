640a14e0498a872ae35fb0d8143295a47182f778
Ztj <ztj1993@gmail.com>
2020-02-12 20:53:51 +0800

    feat: auto dev file

--- src/development/index.js
+++ src/development/index.js
@@ -1,3 +1,10 @@
+let modules = [];
+
 if (process.env.NODE_ENV === 'development') {
-    //TODO: 引入当前目录下除了 index 的所有文件
+    let files = require.context('./', false, /.dev./);
+    files.keys().map(key => {
+        modules.push(files(key).default)
+    });
 }
+
+export default modules;
