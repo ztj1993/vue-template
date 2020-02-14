ac1d3fcea49edd22b5231bda440873f27de0ea3a
Ztj <ztj1993@gmail.com>
2020-02-14 11:55:59 +0800

    optimize: webpack proxy

--- vue.config.js
+++ vue.config.js
@@ -16,16 +16,24 @@ entries.forEach(function (entry) {
     }
 });
 
+let proxy = {};
+let proxy_target = process.env.PROXY_TARGET;
+let proxy_prefix = process.env.PROXY_PREFIX;
+
+if (proxy_target !== '' && proxy_prefix !== '') {
+    let path_rewrite = {};
+    path_rewrite['^' + proxy_prefix] = '';
+    proxy[proxy_prefix] = {
+        target: proxy_target,
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: path_rewrite,
+    };
+}
+
 module.exports = {
     devServer: {
-        proxy: {
-            '/base': {
-                target: process.env.PROXY_TARGET_BASE,
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {'^/base': ''},
-            }
-        }
+        proxy: proxy,
     },
     chainWebpack: config => {
         config.module
