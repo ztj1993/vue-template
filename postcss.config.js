073fc19521035eb58edf3dcfbb07831fd424111b
Ztj <ztj1993@gmail.com>
2020-02-19 09:25:52 +0800

    feat: postcss-plugin-px2rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 let px_viewport = require('./modules/config/postcss-px-to-viewport');
+let px_rem = require('./modules/config/postcss-plugin-px2rem');
 
 module.exports = {
     plugins: {
         ...px_viewport,
+        ...px_rem,
     },
 };
