f73ae94b88f8eabce21f659595248626b59d1419
Ztj <ztj1993@gmail.com>
2020-03-14 20:01:46 +0800

    update: modules/definitions/utils.js

--- modules/definitions/utils.js
+++ modules/definitions/utils.js
@@ -3,9 +3,12 @@ var Utils = (function () {
         this.debug = true;
     }
 
-    Utils.prototype.log = function (message) {
-        this.debug && window.console.log(message);
-    };
+    Utils.prototype.log = window.console.log;
+
+    Utils.prototype.error = window.console.error;
+
+    Utils.prototype.warn = window.console.warn;
+
     Utils.prototype.object_to_object = function (source, target) {
         if (!source) return;
         if (typeof (source) !== 'object') return;
@@ -17,6 +20,20 @@ var Utils = (function () {
                 target[key] = source[key];
             }
         });
+    };
+
+    Utils.prototype.set_window_cache = function (name, value) {
+        if (!window.cache) window.cache = {};
+        window.cache[name] = value;
+    };
+
+    Utils.prototype.get_window_cache = function (name) {
+        if (!window.cache) return undefined;
+        if (name in window.cache) {
+            return window.cache[name];
+        } else {
+            return undefined;
+        }
     };
     return Utils;
 }());
