a968a79941528b7834a6dcb0bf1819e7144ba66a
Ztj <ztj1993@gmail.com>
2020-03-14 20:13:40 +0800

    optimize: modules/utils/development.js

--- modules/utils/development.js
+++ modules/utils/development.js
@@ -1,5 +1,12 @@
 if (process.env.NODE_ENV === 'development') {
-    const files = require.context('../../src', true, /\/development\/index\.js$/);
+    let files = null;
+
+    files = require.context('../../src', true, /\/development\/index\.js$/);
+    files.keys().map(key => {
+        files(key)
+    });
+
+    files = require.context('../../src', true, /\/(.+?).development.js$/);
     files.keys().map(key => {
         files(key)
     });
