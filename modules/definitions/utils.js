df373f249fbc55daa92521224a78a061d16169cf
Ztj <ztj1993@gmail.com>
2020-04-19 13:24:49 +0800

    optimize: definitions/utils.js

--- modules/definitions/utils.js
+++ modules/definitions/utils.js
@@ -1,41 +1,37 @@
-var Utils = (function () {
-    function Utils() {
-        this.debug = true;
-    }
+function Utils() {
+}
 
-    Utils.prototype.log = window.console.log;
+Utils.prototype.log = window.console.log;
 
-    Utils.prototype.error = window.console.error;
+Utils.prototype.error = window.console.error;
 
-    Utils.prototype.warn = window.console.warn;
+Utils.prototype.warn = window.console.warn;
 
-    Utils.prototype.object_to_object = function (source, target) {
-        if (!source) return;
-        if (typeof (source) !== 'object') return;
-        if (!target) return;
-        if (typeof (target) !== 'object') return;
-        let keys = Object.keys(target);
-        keys.forEach(key => {
-            if (key in source) {
-                target[key] = source[key];
-            }
-        });
-    };
-
-    Utils.prototype.set_window_cache = function (name, value) {
-        if (!window.cache) window.cache = {};
-        window.cache[name] = value;
-    };
-
-    Utils.prototype.get_window_cache = function (name) {
-        if (!window.cache) return undefined;
-        if (name in window.cache) {
-            return window.cache[name];
-        } else {
-            return undefined;
+Utils.prototype.object_to_object = function (source, target) {
+    if (!source) return;
+    if (typeof (source) !== 'object') return;
+    if (!target) return;
+    if (typeof (target) !== 'object') return;
+    let keys = Object.keys(target);
+    keys.forEach(key => {
+        if (key in source) {
+            target[key] = source[key];
         }
-    };
-    return Utils;
-}());
+    });
+};
+
+Utils.prototype.set_window_cache = function (name, value) {
+    if (!window.cache) window.cache = {};
+    window.cache[name] = value;
+};
+
+Utils.prototype.get_window_cache = function (name) {
+    if (!window.cache) return undefined;
+    if (name in window.cache) {
+        return window.cache[name];
+    } else {
+        return undefined;
+    }
+};
 
 module.exports = new Utils();
