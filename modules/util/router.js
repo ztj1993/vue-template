2a2e6263547233d296063b35d2fb180877ae26d8
Ztj <ztj1993@gmail.com>
2020-04-19 13:22:14 +0800

    refactor: util/router.js

--- modules/util/router.js
+++ modules/util/router.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
-import RouterPlugin from '../vue/router';
+import RouterPlugin from '../plugin/router';
 
-let files = null;
-let routes = [];
+export default {
+    init: (files) => {
+        const routes = [];
+        files && files.keys().map(key => {
+            routes.push(...files(key).default)
+        });
+        Vue.use(RouterPlugin, {routes: routes});
+    },
+};
 
-files = require.context('../../src', true, /\/router.js$/);
-files.keys().map(key => {
-    routes.push(...files(key).default)
-});
-
-files = require.context('../../src', true, /\/(.+?).router.js$/);
-files.keys().map(key => {
-    routes.push(...files(key).default)
-});
-
-Vue.use(RouterPlugin, {routes: routes});
+// const files = require.context('../../src', true, /\/router.js$/);
+// const files = require.context('../../src', true, /\/(.+?).router.js$/);
