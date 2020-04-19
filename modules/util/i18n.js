abe190a626b2b9096ea9ccccd72d4744b180d8ec
Ztj <ztj1993@gmail.com>
2020-04-19 13:22:07 +0800

    refactor: util/i18n.js

--- modules/util/i18n.js
+++ modules/util/i18n.js
@@ -1,20 +1,24 @@
 import Vue from 'vue'
-import I18nPlugin from "../../modules/vue/i18n";
+import I18nPlugin from "../plugin/i18n";
 
-const files = require.context('../../src', true, /\/locales\/(.*)[a-z]{2}-[A-Z]{2}\.json$/);
+export default {
+    init: (files) => {
+        let messages = {};
+        files && files.keys().map(file => {
+            let locale = file.substr(-10, 5);
+            messages[locale] = files(file)
+        });
 
-let messages = {};
-files.keys().map(file => {
-    let locale = file.substr(-10, 5);
-    messages[locale] = files(file)
-});
+        let options = {
+            locale: process.env.VUE_APP_I18N_LOCALE || 'zh-CN',
+            fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'zh-CN',
+            silentTranslationWarn: true,
+            formatFallbackMessages: true,
+            messages: messages,
+        };
 
-let options = {
-    locale: process.env.VUE_APP_I18N_LOCALE || 'zh-CN',
-    fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'zh-CN',
-    silentTranslationWarn: true,
-    formatFallbackMessages: true,
-    messages: messages,
+        Vue.use(I18nPlugin, options);
+    },
 };
 
-Vue.use(I18nPlugin, options);
+//const files = require.context('../../src', true, /\/locales\/(.*)[a-z]{2}-[A-Z]{2}\.json$/);
