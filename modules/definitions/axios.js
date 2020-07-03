a3980fd4a1ef198ee4d72164a550f41143ddbed3
Ztj <ztj1993@gmail.com>
2020-07-03 22:04:17 +0800

    切换 modules

--- modules/definitions/axios.js
+++
@@ -1,6 +0,0 @@
-const axios = require("axios");
-
-axios.defaults.timeout = 10000;
-axios.defaults.baseURL = process.env.VUE_APP_API_URI;
-
-module.exports = axios;
--- modules/plugin/bus.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-let ApiPlugin = {
-    install: () => {
-        Vue.prototype.$bus = new Vue();
-    },
-};
-
-Vue.use(ApiPlugin);
